09441a9ca43c1c5664fedaae91c6be1973657633
TheKoke <damirkoke01@gmail.com>
2023-07-21 12:30:01 +0600

    dSigma file-format presenting

--- src/business/encoding.py
+++ src/business/encoding.py
@@ -1,8 +1,32 @@
 import struct
 import numpy as np
 
-from business.matrix import Matrix, Spectrum, Locus
+from business.matrix import Matrix, Spectrum, Locus, NucleiConverter
 
+
+BEAM_CHARGE = 0
+BEAM_NUCLON = 1
+TARGET_CHARGE = 2
+TARGET_NUCLON = 3
+BEAM_ENERGY = 4
+DETECTOR_ANGLE = 8
+
+E_DETECTOR_THICKNESS = 12
+E_DETECTOR_MADEOF = 16
+DE_DETECTOR_THICKNESS = 20
+DE_DETECTOR_RHICKNESS = 24
+
+INTEGRATOR_COUNTS = 28
+CONGRUENCE = 32
+INTEGRATOR_CONSTANT = 36
+COLLIMATOR_RADIUS = 40
+TARGET_DETECTOR_DISTANCE = 44
+
+E_SIZE = 48
+DE_SIZE = 50
+MATRIX_START = 52
+
+LOCUSES_START = lambda height, width: 4 * height * width
 
 class Encoder:
     def __init__(self, matrix: Matrix, directory: str) -> None:
@@ -14,12 +38,17 @@ class Encoder:
         buffer = open(path, 'wb')
 
     def generate_file_name(self) -> str:
-        pass
+        beam_name = NucleiConverter.to_string(self.matrix.experiment.beam)
+        target_name = NucleiConverter.to_string(self.matrix.experiment.target)
+        energy = self.matrix.experiment.beam_energy
+        angle = self.matrix.angle
+
+        return f'{target_name}+{beam_name}_{round(energy, 1)}MeV_{angle}.ds'
 
 
 class Decoder:
     def __init__(self, path: str) -> None:
-        self.buffer = open(path, 'rb')
+        self.buffer = open(path, 'rb').read()
 
 
 if __name__ == '__main__':
